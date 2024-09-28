e5a5490da6079f3af445ba44a2c563c5aa9f9941
Phungvie <phungtienviet14@gmail.com>
2024-09-28 17:07:02 +0700

    Điểm khởi đầu

---
+++ README.md
@@ -0,0 +1,9 @@
+# BanTaiKhoanNgheNhac
+Đầu tiên cài Docker Desktop
+
+# Install Kafka
+docker-compose là file trong source code. cd đến thư mục chứa file docker-compose rồi chay lệnh:
+`docker-compose up -d`
+
+# Postman
+có file postman_collection.json anh em nhớ input lên postman nhé
---
+++ profile-service/src/main/java/com/devteria/notification/controller/InternalUserProfileController.java
@@ -0,0 +1,28 @@
+package com.devteria.notification.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devteria.notification.dto.ApiResponse;
+import com.devteria.notification.dto.request.ProfileCreationRequest;
+import com.devteria.notification.dto.response.UserProfileResponse;
+import com.devteria.notification.service.UserProfileService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class InternalUserProfileController {
+    UserProfileService userProfileService;
+
+    @PostMapping("/internal/users")
+    ApiResponse<UserProfileResponse> createProfile(@RequestBody ProfileCreationRequest request) {
+        return ApiResponse.<UserProfileResponse>builder()
+                .result(userProfileService.createProfile(request))
+                .build();
+    }
+}
